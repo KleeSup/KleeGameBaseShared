4c8aceda9234c420a36ac294044c544a02a1db06
KleeSup <45318360+KleeSup@users.noreply.github.com>
2023-01-30 18:38:51 +0100

    Added Velocity2D.java and Velocity3D.java as wrapper classes for Vector2 and Vector3 from LibGDX

---
+++ src/main/java/de/kleesup/libraries/gamebase/shared/math/Velocity3D.java
@@ -0,0 +1,33 @@
+package de.kleesup.libraries.gamebase.shared.math;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * A wrapper class of {@link Vector3} from LibGDX.
+ * <br>Created on 30.01.2023</br>
+ * @author KleeSup
+ * @version 1.0
+ * @since 1.1.3
+ */
+public class Velocity3D extends Vector3 {
+
+    public Velocity3D() {
+    }
+
+    public Velocity3D(float x, float y, float z) {
+        super(x, y, z);
+    }
+
+    public Velocity3D(Vector3 vector) {
+        super(vector);
+    }
+
+    public Velocity3D(float[] values) {
+        super(values);
+    }
+
+    public Velocity3D(Vector2 vector, float z) {
+        super(vector, z);
+    }
+}

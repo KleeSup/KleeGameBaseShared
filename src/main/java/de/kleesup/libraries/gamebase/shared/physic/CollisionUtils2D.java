d5ea44ad2b1a5c041d59ebccaa56fabbbc971350
KleeSup <45318360+kleesup@users.noreply.github.com>
2023-04-16 20:44:18 +0200

    Version 1.1.6, some additions and critical fixes to CollisionUtils2D

--- src/main/java/de/kleesup/libraries/gamebase/shared/physic/CollisionUtils2D.java
+++ src/main/java/de/kleesup/libraries/gamebase/shared/physic/CollisionUtils2D.java
@@ -10,7 +10,7 @@ import de.kleesup.libraries.gamebase.shared.math.gdx.AABB2D;
  * Note for authors: Remember that the C-code from Falconerd uses AABB.position as the center of the AABB not the bottom left corner!!
  * <br>Created on 12.02.2023</br>
  * @author KleeSup
- * @version 1.1
+ * @version 1.2
  * @since 1.1.4
  */
 public class CollisionUtils2D {
@@ -122,25 +122,67 @@ public class CollisionUtils2D {
      */
 
     /**
+     * This method checks for collisions DIRECTLY between two AABBs.
+     * NOTE: this method does not solve the tunneling problem, so it will not detect certain collisions.
+     * If this is an issue, use {@link #sweptAABBvsAABB(AABB2D, AABB2D, float, float, AABB2D)})}.
+     * @param aabb The first AABB (this could be the one moving into the other).
+     * @param other The other AABB (could be the one which the first one moves into).
+     * @param goalX The x-coordinate of the goal position where the first AABB is supposed to be.
+     * @param goalY The y-coordinate of the goal position where the first AABB is supposed to be.
+     * @param responseWriteTo A temp response object where the outcome can be written into (Useful when less object creation is desired).
+     * @return The result of the AABB collision test.
+     */
+    public static SweptResponse quickAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY, SweptResponse responseWriteTo, boolean useTempVec){
+        KleeUtil.paramRequireNonNull(aabb, "First AABB cannot be null!");
+        KleeUtil.paramRequireNonNull(other, "Second AABB cannot be null!");
+        return doesRayIntersectAABB(aabb.getCenterX(), aabb.getCenterY(), goalX, goalY, other, responseWriteTo, useTempVec);
+    }
+    public static SweptResponse quickAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY, boolean useTempVec){
+        return quickAABBvsAABB(aabb,other,goalX,goalY,null,useTempVec);
+    }
+    public static SweptResponse quickAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY){
+        return quickAABBvsAABB(aabb,other,goalX,goalY,null,false);
+    }
+
+    /**
+     * Does a quick collision test between two AABBs. However, this method expects that the first AABB has already been moved.
+     * @param aabb The first AABB (this could be the one moving into the other).
+     * @param other The other AABB (could be the one which the first one moves into).
+     * @return The result of the collision test.
+     */
+    public static SweptResponse quickAABBvsAABB(AABB2D aabb, AABB2D other){
+        return quickAABBvsAABB(aabb,other,aabb.getCenterX(),aabb.getCenterY());
+    }
+
+    /**
      * Does a swept collision test between two AABBs.
+     * NOTE: The way how the math behind all this fixes the tunneling problem is by creating a summed AABB (centering the <b>other</b> one and adding both sizes).
+     * Therefore, the returned x and y values of the {@link SweptResponse} do no longer represent the hit position at the original <b>other</b> AABB
+     * but the hit position of the <b>aabb</b> at the <b>sumWriteTo</b>.
+     * Keep this in mind when doing collision resolution (See {@link #sweptAABBvsAABBResolution(AABB2D, SweptResponse)}).
      * @param aabb The first AABB (this could be the one moving into the other).
      * @param other The other AABB (could be the one which the first one moves into).
      * @param goalX The x-coordinate of the goal position where the first AABB is supposed to be.
      * @param goalY The y-coordinate of the goal position where the first AABB is supposed to be.
      * @param sumWriteTo A temp AABB object where the sum between the first and the second can be written into (Useful when less object creation is desired).
      * @param responseWriteTo A temp response object where the outcome can be written into (Useful when less object creation is desired).
+     * @param useTempVec Determines whether a temp {@link Vector2} object is supposed to be used
+     *                   (Useful when less object creation is desired but also disables multithreading for this method at the same time).
      * @return The result of the swept collision test.
      */
-    public static SweptResponse sweptAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY, AABB2D sumWriteTo, SweptResponse responseWriteTo){
+    public static SweptResponse sweptAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY, AABB2D sumWriteTo, SweptResponse responseWriteTo, boolean useTempVec){
         KleeUtil.paramRequireNonNull(aabb, "First AABB cannot be null!");
         KleeUtil.paramRequireNonNull(other, "Second AABB cannot be null!");
-        if(sumWriteTo == null){
-            //calculating the sum of both AABBs by adding their width and height together.
-            float width = aabb.getWidth() + other.getWidth();
-            float height = aabb.getHeight() + other.getHeight();
-            sumWriteTo = new AABB2D(aabb.getX() - (width/2f), aabb.getY() - (height/2f), aabb.getWidth() + width, aabb.getHeight() + height);
-        }
-        return doesRayIntersectAABB(aabb.getCenterX(), aabb.getCenterY(), goalX, goalY, sumWriteTo, responseWriteTo);
+        if(sumWriteTo == null)sumWriteTo = new AABB2D();
+        //calculating the sum of both AABBs by adding their width and height together.
+        float width = aabb.getWidth() + other.getWidth();
+        float height = aabb.getHeight() + other.getHeight();
+        sumWriteTo.setSize(width, height);
+        sumWriteTo.setCenter(other.getX(), other.getY());
+        return doesRayIntersectAABB(aabb.getCenterX(), aabb.getCenterY(), goalX, goalY, sumWriteTo, responseWriteTo, useTempVec);
+    }
+    public static SweptResponse sweptAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY, AABB2D sumWriteTo, SweptResponse responseWriteTo){
+        return sweptAABBvsAABB(aabb,other,goalX,goalY,sumWriteTo,responseWriteTo,false);
     }
     public static SweptResponse sweptAABBvsAABB(AABB2D aabb, AABB2D other, float goalX, float goalY, AABB2D sumWriteTo){
         return sweptAABBvsAABB(aabb, other, goalX, goalY, sumWriteTo, null);
@@ -151,6 +193,7 @@ public class CollisionUtils2D {
 
     /**
      * Does a swept collision test between two AABBs. However, this method expects that the first AABB has already been moved.
+     * NOTE: Since the AABB has already been moved, the swept test isn't really necessary so rather use {@link #quickAABBvsAABB(AABB2D, AABB2D)}.
      * @param aabb The first AABB (this could be the one moving into the other).
      * @param other The other AABB (could be the one which the first one moves into).
      * @return The result of the swept collision test.
@@ -250,5 +293,52 @@ public class CollisionUtils2D {
         return doesRayIntersectAABB(x,y,magnitude,aabb,null);
     }
 
+    /*
+    Resolution
+    */
+
+    /**
+     * Calculates a goal-AABB which represents the original AABB moved away from the AABB it collided with.
+     * NOTE: This method expects that the response was created between the two AABBs directly and no summed AABB
+     * (like in {@link #sweptAABBvsAABB(AABB2D, AABB2D, float, float, AABB2D)}) is involved.
+     * @param aabb The aabb which was tested for collision with another one.
+     * @param response The response that was returned by that test.
+     * @param goalWriteTo The goal-AABB to write to (Useful when less object creation is desired).
+     * @return The goal-AABB representing the moved  <b>aabb</b> to no longer collide.
+     */
+    public AABB2D quickAABBvsAABBResolution(AABB2D aabb, SweptResponse response, AABB2D goalWriteTo){
+        goalWriteTo.setSize(aabb.getWidth(), aabb.getHeight());
+
+        //response
+        float halfWidth = aabb.getWidth()/2f;
+        float halfHeight = aabb.getHeight()/2f;
+
+        float moveOutX = halfWidth * response.normalX;
+        float moveOutY = halfHeight * response.normalY;
+
+        goalWriteTo.setCenter(response.x + moveOutX, response.y + moveOutY);
+        return goalWriteTo;
+    }
+    public AABB2D quickAABBvsAABBResolution(AABB2D aabb, SweptResponse response){
+        return quickAABBvsAABBResolution(aabb, response, new AABB2D());
+    }
+
+    /**
+     * Calculates a goal-AABB which represents the original AABB moved away from the AABB it collided with.
+     * NOTE: This method expects that the response was created by a swept test that utilises a summed AABB of both sizes to
+     * fix the tunneling problem.
+     * @param aabb The aabb which was tested for collision with another one.
+     * @param response The response that was returned by that test.
+     * @param goalWriteTo The goal-AABB to write to (Useful when less object creation is desired).
+     * @return The goal-AABB representing the moved <b>aabb</b> to no longer collide.
+     */
+    public AABB2D sweptAABBvsAABBResolution(AABB2D aabb, SweptResponse response, AABB2D goalWriteTo){
+        goalWriteTo.setSize(aabb.getWidth(), aabb.getHeight());
+        goalWriteTo.setCenter(response.x, response.y); //response-x and y are no longer the
+        return goalWriteTo;
+    }
+    public AABB2D sweptAABBvsAABBResolution(AABB2D aabb, SweptResponse response){
+        return sweptAABBvsAABBResolution(aabb,response,new AABB2D());
+    }
 
 }
